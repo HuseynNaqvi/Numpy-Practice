939911f39c7325d5b6d9eb18512621c220b875b8
Huseyn <hussainsn13.11@gmail.com>
2026-07-05 13:20:07 +0500

    learned multi-dimensional arrays

---
+++ intro.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+array = np.array([[['A', 'B', 'C'],['D', 'E', 'F'],['G', 'H', 'I']],
+                  [['J', 'K', 'L'],['M', 'N', 'O'],['P', 'Q', 'R']]])
+
+word = array[0, 0, 0] + array[1, 1, 1] + array[1, 0, 1] + array[1, 0, 2] + array[0, 1, 1]
+
+print(word)
